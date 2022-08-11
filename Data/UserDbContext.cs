bc91fe86469a017a6c8575f0c99fbd5ab05b6422
Wagner <wagner_silvajr@hotmail.com>
2022-08-11 16:56:25 -0300

    feat: added class for communication with db

---
+++ Data/UserDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace user.Data
+{
+    public class UserDbContext
+    {
+        
+    }
+}
