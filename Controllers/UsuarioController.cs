17c4b0ba368fe88181415aa439f0cc0b6845d1e8
Wagner <wagner_silvajr@hotmail.com>
2022-08-12 19:28:41 -0300

    Fix: expression lambda on List

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,30 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using user.Model;
 
-namespace usuario.Controllers
+namespace user.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        private static List<User> Users()
-        {
-            return new List<User>
+        private static List<User> Users => new List<User>
             {
                 new User {Id = 1, Name = "Wagner", BirthDate = new DateTime(1995/10/28)}
             };
-        }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(Users());
+            return Ok(Users);
         }
 
         [HttpPost]
         public IActionResult Post(User user)
         {
-            var users = Users();
+            var users = Users;
             users.Add(user);
             return Ok(users);
         }
