0f535504eb3cef0ea7d384cbf13a5bb4b6e553c6
Wagner <wagner_silvajr@hotmail.com>
2022-08-29 18:46:19 -0300

    feat: changes o repository

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using user.Data;
+
 using user.Model;
 using user.Repository.Interfaces;
 
@@ -10,10 +10,10 @@ namespace user.Repository
 {
     public class UserRepository : BaseRepository<User>, IUserRepository
     {        
-        private readonly IUserRepository _context;
-        public UserRepository(UserDbContext context) : base ()
+        private readonly IUserRepository _userRepository;
+        public UserRepository(IUserRepository userRepository) : base ()
         {
-            this._context = context;         
+            this._userRepository = userRepository;         
         }
        
         public Task<User> GetByName(string name)
